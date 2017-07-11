6b3a7ad7454777fec95173a9fc71ee545655fe62
Johns, Robert <rjohns@visa.com>
2017-07-11 12:25:04 -0500

    Initial commit

---
+++ AdamPodTest/MyButton.swift
@@ -0,0 +1,13 @@
+//
+//  MyButton.swift
+//  AdamPodTest
+//
+//  Created by Johns, Robert on 7/11/17.
+//  Copyright © 2017 Visa. All rights reserved.
+//
+
+import UIKit
+
+class MyButton: UIControl {
+
+}

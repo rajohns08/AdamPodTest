b86185cbc84c265e06446d7ee3b71c09dc6d6b6b
Johns, Robert <rjohns@visa.com>
2017-07-11 14:48:18 -0500

    Update pod and add framework

---
+++ AdamPodTest.framework/Headers/AdamPodTest.h
@@ -0,0 +1,19 @@
+//
+//  AdamPodTest.h
+//  AdamPodTest
+//
+//  Created by Johns, Robert on 7/11/17.
+//  Copyright © 2017 Visa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AdamPodTest.
+FOUNDATION_EXPORT double AdamPodTestVersionNumber;
+
+//! Project version string for AdamPodTest.
+FOUNDATION_EXPORT const unsigned char AdamPodTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AdamPodTest/PublicHeader.h>
+
+
--- AdamPodTest.podspec
+++ AdamPodTest.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name  = "AdamPodTest"
-  s.version = "0.0.2"
+  s.version = "0.0.3"
   s.summary = "A short description of AdamPodTest."
   s.homepage = "http://EXAMPLE/AdamPodTest"
   s.license = "Do whatever you want WTFPL"
